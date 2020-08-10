26fe39e054b2ce335b0404b5b0b1ac0a2e35ea54
Bryam Vicente <vicentebryam@gmail.com>
2020-08-10 02:31:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,7 +26,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
- array["#{index_number}]
+ array
 
 end
 
@@ -36,6 +36,7 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
+  array[-1]
 
 end
 
